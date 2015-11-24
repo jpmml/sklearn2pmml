f0e8803ed11a75ba9ef51d973dffee0fdba89c8b
ybrovman <ybrovman@ebay.com>
2015-11-24 16:23:05 -0500

    fixed spacing

--- sklearn2pmml/__init__.py
+++ sklearn2pmml/__init__.py
@@ -13,37 +13,37 @@ __copyright__ = "Copyright (c) 2015 Villu Ruusmann"
 __license__ = "GNU Affero General Public License (AGPL) version 3.0"
 
 def _classpath():
-	jars = []
-	resources = pkg_resources.resource_listdir("sklearn2pmml.resources", "")
-	for resource in resources:
-		if(resource.endswith(".jar")):
-			jars.append(pkg_resources.resource_filename("sklearn2pmml.resources", resource))
-	return jars
+    jars = []
+    resources = pkg_resources.resource_listdir("sklearn2pmml.resources", "")
+    for resource in resources:
+        if(resource.endswith(".jar")):
+            jars.append(pkg_resources.resource_filename("sklearn2pmml.resources", resource))
+    return jars
 
 def _dump(obj):
-	fd, path = tempfile.mkstemp(suffix = ".pkl")
-	joblib.dump(obj, path, compress = 9)
-	return path
+    fd, path = tempfile.mkstemp(suffix = ".pkl")
+    joblib.dump(obj, path, compress = 9)
+    return path
 
 def sklearn2pmml(estimator, mapper, pmml, verbose = False):
-	if(isinstance(estimator, BaseEstimator) is False):
-		raise TypeError()
-	if((mapper is not None) and (isinstance(mapper, DataFrameMapper) is False)):
-		raise TypeError()
-	cmd = ["java", "-cp", os.pathsep.join(_classpath()), "org.jpmml.sklearn.Main"]
-	dumps = []
-	try:
-		estimator_pkl = _dump(estimator)
-		cmd.extend(["--pkl-estimator-input", estimator_pkl])
-		dumps.append(estimator_pkl)
-		if(mapper):
-			mapper_pkl = _dump(mapper)
-			cmd.extend(["--pkl-mapper-input", mapper_pkl])
-			dumps.append(mapper_pkl)
-		cmd.extend(["--pmml-output", pmml])
-		if(verbose): 
+    if(isinstance(estimator, BaseEstimator) is False):
+        raise TypeError()
+    if((mapper is not None) and (isinstance(mapper, DataFrameMapper) is False)):
+        raise TypeError()
+    cmd = ["java", "-cp", os.pathsep.join(_classpath()), "org.jpmml.sklearn.Main"]
+    dumps = []
+    try:
+        estimator_pkl = _dump(estimator)
+        cmd.extend(["--pkl-estimator-input", estimator_pkl])
+        dumps.append(estimator_pkl)
+        if(mapper):
+            mapper_pkl = _dump(mapper)
+            cmd.extend(["--pkl-mapper-input", mapper_pkl])
+            dumps.append(mapper_pkl)
+        cmd.extend(["--pmml-output", pmml])
+        if(verbose):
             print(cmd)
-		subprocess.check_call(cmd)
-	finally:
-		for dump in dumps:
-			os.remove(dump)
+        subprocess.check_call(cmd)
+    finally:
+        for dump in dumps:
+            os.remove(dump)
