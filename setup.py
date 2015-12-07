6752a4c99eb160f049186f6a8fa267242478fd6d
Villu Ruusmann <villu.ruusmann@gmail.com>
2015-12-07 18:09:50 +0200

    Updated JPMML-SkLearn dependency

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
 	name = "sklearn2pmml",
-	version = "0.5.1",
+	version = "0.5.2",
 	description = "Python library for converting Scikit-Learn models to PMML",
 	author = "Villu Ruusmann",
 	author_email = "villu.ruusmann@gmail.com",
