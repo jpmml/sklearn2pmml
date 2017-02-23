f93323853c9888e94a5fb479b4af8a7bb5270c5c
Villu Ruusmann <villu.ruusmann@gmail.com>
2017-02-24 00:18:27 +0200

    Updated JPMML-SkLearn dependency

--- sklearn2pmml/__init__.py
+++ sklearn2pmml/__init__.py
@@ -18,7 +18,7 @@ import tempfile
 
 __copyright__ = "Copyright (c) 2015 Villu Ruusmann"
 __license__ = "GNU Affero General Public License (AGPL) version 3.0"
-__version__ = "0.17.2"
+__version__ = "0.17.3"
 
 class PMMLPipeline(Pipeline):
 
