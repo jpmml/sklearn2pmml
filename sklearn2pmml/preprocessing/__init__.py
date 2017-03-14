59d36a656009a7e932675f0bb40649765f47e046
Villu Ruusmann <villu.ruusmann@gmail.com>
2017-03-14 18:11:40 +0200

    Added 'PMMLLabelBinarizer' transformation type

--- sklearn2pmml/preprocessing/__init__.py
+++ sklearn2pmml/preprocessing/__init__.py
@@ -5,6 +5,22 @@ from sklearn.utils import column_or_1d
 import numpy
 import pandas
 
+class PMMLLabelBinarizer(BaseEstimator, TransformerMixin):
+
+	def fit(self, y):
+		y = column_or_1d(y, warn = True)
+		self.classes_ = numpy.unique(y[~pandas.isnull(y)])
+		return self
+
+	def transform(self, y):
+		y = column_or_1d(y, warn = True)
+		index = list(self.classes_)
+		Y = numpy.zeros((len(y), len(index)), dtype = numpy.int)
+		for i, v in enumerate(y):
+			if not pandas.isnull(v):
+				Y[i, index.index(v)] = 1
+		return Y
+
 class PMMLLabelEncoder(BaseEstimator, TransformerMixin):
 
 	def __init__(self, missing_value = None):
--- sklearn2pmml/preprocessing/tests/__init__.py
+++ sklearn2pmml/preprocessing/tests/__init__.py
@@ -1,8 +1,43 @@
 from pandas import Series
-from sklearn2pmml.preprocessing import PMMLLabelEncoder
+from sklearn2pmml.preprocessing import PMMLLabelBinarizer, PMMLLabelEncoder
 from unittest import TestCase
 
 import numpy
+
+class PMMLLabelBinarizerTest(TestCase):
+
+	def test_fit_float(self):
+		y = [1.0, float("NaN"), 1.0, 2.0, float("NaN"), 3.0, 3.0, 2.0]
+		labels = [1.0, 2.0, 3.0]
+		binarizer = PMMLLabelBinarizer()
+		binarizer.fit(y)
+		self.assertEqual(labels, binarizer.classes_.tolist())
+
+	def test_fit_string(self):
+		y = ["A", None, "A", "B", None, "C", "C", "B"]
+		labels = ["A", "B", "C"]
+		binarizer = PMMLLabelBinarizer()
+		self.assertFalse(hasattr(binarizer, "classes_"))
+		binarizer.fit(y)
+		self.assertEqual(labels, binarizer.classes_.tolist())
+		binarizer.fit(numpy.array(y))
+		self.assertEqual(labels, binarizer.classes_.tolist())
+		binarizer.fit(Series(numpy.array(y)))
+		self.assertEqual(labels, binarizer.classes_.tolist())
+
+	def test_transform_float(self):
+		y = [1.0, float("NaN"), 2.0, 3.0]
+		binarizer = PMMLLabelBinarizer()
+		binarizer.fit(y)
+		self.assertEqual(numpy.array([[1, 0, 0], [0, 0, 1], [0, 0, 0], [0, 1, 0]]).tolist(), binarizer.transform([1.0, 3.0, float("NaN"), 2.0]).tolist())
+
+	def test_transform_string(self):
+		y = ["A", None, "B", "C"]
+		binarizer = PMMLLabelBinarizer()
+		binarizer.fit(y)
+		self.assertEqual(numpy.array([[1, 0, 0], [0, 0, 1], [0, 0, 0], [0, 1, 0]]).tolist(), binarizer.transform(["A", "C", None, "B"]).tolist())
+		self.assertEqual(numpy.array([[0, 0, 0]]).tolist(), binarizer.transform([None]).tolist())
+		self.assertEqual(numpy.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]]).tolist(), binarizer.transform(["A", "B", "C"]).tolist())
 
 class PMMLLabelEncoderTest(TestCase):
 
