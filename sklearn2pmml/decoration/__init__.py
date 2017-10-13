ca397fb3cfbdfd8487976ef273748b16efb9b242
Villu Ruusmann <villu.ruusmann@gmail.com>
2017-10-13 10:31:24 +0300

    Added 'Domain.missing_values' attribute

--- sklearn2pmml/decoration/__init__.py
+++ sklearn2pmml/decoration/__init__.py
@@ -7,17 +7,19 @@ import pandas
 def _count(mask):
 	if hasattr(mask, "values"):
 		mask = mask.values
-	non_missing_freq = sum(mask)
-	missing_freq = sum(~mask)
+	missing_freq = sum(mask)
+	non_missing_freq = sum(~mask)
 	return {
-		"totalFreq" : (non_missing_freq + missing_freq),
+		"totalFreq" : (missing_freq + non_missing_freq),
 		"missingFreq" : missing_freq,
 		"invalidFreq" : (non_missing_freq - non_missing_freq) # A scalar zero, or an array of zeroes
 	}
 
 class Domain(BaseEstimator, TransformerMixin):
 
-	def __init__(self, missing_value_treatment = "as_is", missing_value_replacement = None, invalid_value_treatment = "return_invalid", invalid_value_replacement = None, with_data = True, with_statistics = True):
+	def __init__(self, missing_values = None, missing_value_treatment = "as_is", missing_value_replacement = None, invalid_value_treatment = "return_invalid", invalid_value_replacement = None, with_data = True, with_statistics = True):
+		if missing_values is not None:
+			self.missing_values = missing_values
 		missing_value_treatments = ["as_is", "as_mean", "as_mode", "as_median", "as_value"]
 		if missing_value_treatment not in missing_value_treatments:
 			raise ValueError("Missing value treatment {0} not in {1}".format(missing_value_treatment, missing_value_treatments))
@@ -33,12 +35,19 @@ class Domain(BaseEstimator, TransformerMixin):
 		self.with_data = with_data
 		self.with_statistics = with_statistics
 
+	def _empty_fit(self):
+		return not (self.with_data or self.with_statistics)
+
+	def _get_mask(self, X):
+		if hasattr(self, "missing_values"):
+			return X == self.missing_values
+		else:
+			return pandas.isnull(X)
+
 	def transform(self, X):
 		if hasattr(self, "missing_value_replacement"):
-			if hasattr(X, "fillna"):
-				X.fillna(value = self.missing_value_replacement, inplace = True)
-			else:
-				X[pandas.isnull(X)] = self.missing_value_replacement
+			mask = self._get_mask(X)
+			X[mask] = self.missing_value_replacement
 		return X
 
 class CategoricalDomain(Domain):
@@ -48,8 +57,10 @@ class CategoricalDomain(Domain):
 
 	def fit(self, X, y = None):
 		X = column_or_1d(X, warn = True)
-		mask = pandas.notnull(X)
-		values, counts = numpy.unique(X[mask], return_counts = True)
+		if self._empty_fit():
+			return self
+		mask = self._get_mask(X)
+		values, counts = numpy.unique(X[~mask], return_counts = True)
 		if self.with_data:
 			self.data_ = values
 		if self.with_statistics:
@@ -62,21 +73,29 @@ class ContinuousDomain(Domain):
 	def __init__(self, **kwargs):
 		super(ContinuousDomain, self).__init__(**kwargs)
 
+	def _interquartile_range(self, X, axis):
+		quartiles = numpy.nanpercentile(X, [25, 75], axis = axis)
+		return (quartiles[1] - quartiles[0])
+
 	def fit(self, X, y = None):
-		mask = pandas.notnull(X)
-		min = numpy.nanmin(X, axis = 0)
-		max = numpy.nanmax(X, axis = 0)
+		if self._empty_fit():
+			return self
+		mask = self._get_mask(X)
+		X = numpy.ma.masked_array(X, mask = mask)
+		min = numpy.asarray(numpy.nanmin(X, axis = 0))
+		max = numpy.asarray(numpy.nanmax(X, axis = 0))
 		if self.with_data:
 			self.data_min_ = min
 			self.data_max_ = max
 		if self.with_statistics:
 			self.counts_ = _count(mask)
+			X = numpy.ma.asarray(X, dtype = numpy.float).filled(float('NaN'))
 			self.numeric_info_ = {
 				"minimum" : min,
 				"maximum" : max,
-				"mean" : numpy.nanmean(X, axis = 0),
-				"standardDeviation" : numpy.nanstd(X, axis = 0),
-				"median" : numpy.nanmedian(X, axis = 0),
-				"interQuartileRange" : (numpy.nanpercentile(X, 75, axis = 0) - numpy.nanpercentile(X, 25, axis = 0))
+				"mean" : numpy.asarray(numpy.nanmean(X, axis = 0)),
+				"standardDeviation" : numpy.asarray(numpy.nanstd(X, axis = 0)),
+				"median" : numpy.asarray(numpy.nanmedian(X, axis = 0)),
+				"interQuartileRange" : numpy.asarray(self._interquartile_range(X, axis = 0))
 			}
 		return self
--- sklearn2pmml/decoration/tests/__init__.py
+++ sklearn2pmml/decoration/tests/__init__.py
@@ -15,7 +15,10 @@ def _array_to_list(info):
 class CategoricalDomainTest(TestCase):
 
 	def test_fit_int(self):
+		domain = CategoricalDomain(with_data = False, with_statistics = False)
+		self.assertTrue(domain._empty_fit())
 		domain = CategoricalDomain(missing_value_treatment = "as_value", missing_value_replacement = -999, invalid_value_treatment = "as_is", invalid_value_replacement = 0)
+		self.assertFalse(hasattr(domain, "missing_values_"))
 		self.assertEqual("as_value", domain.missing_value_treatment)
 		self.assertEqual(-999, domain.missing_value_replacement)
 		self.assertEqual("as_is", domain.invalid_value_treatment)
@@ -23,6 +26,7 @@ class CategoricalDomainTest(TestCase):
 		self.assertFalse(hasattr(domain, "data_"))
 		self.assertFalse(hasattr(domain, "counts_"))
 		self.assertFalse(hasattr(domain, "discr_stats_"))
+		self.assertFalse(domain._empty_fit())
 		X = DataFrame(numpy.array([1, None, 3, 2, None, 2]))
 		Xt = domain.fit_transform(X)
 		self.assertEqual(numpy.array([1, 2, 3]).tolist(), domain.data_.tolist())
@@ -33,18 +37,55 @@ class CategoricalDomainTest(TestCase):
 		Xt = domain.transform(X)
 		self.assertEqual(numpy.array([-999, -999]).tolist(), Xt.tolist())
 
+	def test_fit_int_missing(self):
+		domain = CategoricalDomain(missing_values = -1, missing_value_replacement = 0)
+		self.assertEqual(-1, domain.missing_values)
+		self.assertEqual(0, domain.missing_value_replacement)
+		self.assertFalse(domain._empty_fit())
+		X = DataFrame(numpy.array([1, -1, 3, 2, -1, 2]))
+		Xt = domain.fit_transform(X)
+		self.assertEqual(numpy.array([1, 2, 3]).tolist(), domain.data_.tolist())
+		self.assertEqual({"totalFreq" : 6, "missingFreq" : 2, "invalidFreq" : 0}, domain.counts_)
+		self.assertEqual({1 : 1, 2 : 2, 3 : 1}, _value_count(domain.discr_stats_))
+		self.assertEqual(numpy.array([1, 0, 3, 2, 0, 2]).tolist(), Xt[0].tolist())
+		X = numpy.array([-1, -1])
+		Xt = domain.transform(X)
+		self.assertEqual(numpy.array([0, 0]).tolist(), Xt.tolist())
+
 	def test_fit_string(self):
-		domain = CategoricalDomain(with_statistics = False)
+		domain = CategoricalDomain(with_data = False, with_statistics = False)
+		self.assertTrue(domain._empty_fit())
+		domain = CategoricalDomain(missing_values = None, with_statistics = False)
+		self.assertFalse(hasattr(domain, "missing_values_"))
 		self.assertEqual("as_is", domain.missing_value_treatment)
 		self.assertFalse(hasattr(domain, "missing_value_replacement"))
 		self.assertEqual("return_invalid", domain.invalid_value_treatment)
 		self.assertFalse(hasattr(domain, "invalid_value_replacement"))
+		self.assertFalse(domain._empty_fit())
 		X = numpy.array(["1", None, "3", "2", None, "2"])
 		Xt = domain.fit_transform(X)
 		self.assertEqual(numpy.array(["1", "2", "3"]).tolist(), domain.data_.tolist())
 		self.assertFalse(hasattr(domain, "counts_"))
 		self.assertFalse(hasattr(domain, "discr_stats_"))
 		self.assertEqual(numpy.array(["1", None, "3", "2", None, "2"]).tolist(), Xt.tolist())
+		X = numpy.array([None, None])
+		Xt = domain.transform(X)
+		self.assertEqual(numpy.array([None, None]).tolist(), Xt.tolist())
+
+	def test_fit_string_missing(self):
+		domain = CategoricalDomain(missing_values = "N/A", missing_value_replacement = "0")
+		self.assertEqual("N/A", domain.missing_values)
+		self.assertEqual("0", domain.missing_value_replacement)
+		self.assertFalse(domain._empty_fit())
+		X = numpy.array(["1", "N/A", "3", "2", "N/A", "2"])
+		Xt = domain.fit_transform(X)
+		self.assertEqual(numpy.array(["1", "2", "3"]).tolist(), domain.data_.tolist())
+		self.assertEqual({"totalFreq" : 6, "missingFreq" : 2, "invalidFreq" : 0}, domain.counts_)
+		self.assertEqual({"1" : 1, "2" : 2, "3" : 1}, _value_count(domain.discr_stats_))
+		self.assertEqual(numpy.array(["1", "0", "3", "2", "0", "2"]).tolist(), Xt.tolist())
+		X = numpy.array(["N/A", "N/A"])
+		Xt = domain.transform(X)
+		self.assertEquals(numpy.array(["0", "0"]).tolist(), Xt.tolist())
 
 	def test_mapper(self):
 		domain = CategoricalDomain()
@@ -61,7 +102,10 @@ class CategoricalDomainTest(TestCase):
 class ContinuousDomainTest(TestCase):
 
 	def test_fit_float(self):
+		domain = ContinuousDomain(with_data = False, with_statistics = False)
+		self.assertTrue(domain._empty_fit())
 		domain = ContinuousDomain(missing_value_treatment = "as_value", missing_value_replacement = -1.0, invalid_value_treatment = "as_is", invalid_value_replacement = 0.0)
+		self.assertFalse(hasattr(domain, "missing_values"))
 		self.assertEqual("as_value", domain.missing_value_treatment)
 		self.assertEqual(-1.0, domain.missing_value_replacement)
 		self.assertEqual("as_is", domain.invalid_value_treatment)
@@ -70,6 +114,7 @@ class ContinuousDomainTest(TestCase):
 		self.assertFalse(hasattr(domain, "data_max_"))
 		self.assertFalse(hasattr(domain, "counts_"))
 		self.assertFalse(hasattr(domain, "numeric_info_"))
+		self.assertFalse(domain._empty_fit())
 		X = DataFrame(numpy.array([1.0, float('NaN'), 3.0, 2.0, float('NaN'), 2.0]))
 		Xt = domain.fit_transform(X)
 		self.assertEqual(1.0, domain.data_min_)
@@ -80,6 +125,22 @@ class ContinuousDomainTest(TestCase):
 		X = numpy.array([float('NaN'), None])
 		Xt = domain.transform(X)
 		self.assertEqual(numpy.array([-1.0, -1.0]).tolist(), Xt.tolist())
+
+	def test_fit_float_missing(self):
+		domain = ContinuousDomain(missing_values = -1.0, missing_value_replacement = 4.0)
+		self.assertEqual(-1.0, domain.missing_values)
+		self.assertEqual(4.0, domain.missing_value_replacement)
+		self.assertFalse(domain._empty_fit())
+		X = DataFrame(numpy.array([1.0, -1.0, 3.0, 2.0, -1.0, 2.0]))
+		Xt = domain.fit_transform(X)
+		self.assertEqual(1.0, domain.data_min_)
+		self.assertEqual(3.0, domain.data_max_)
+		self.assertEqual({"totalFreq" : 6, "missingFreq" : 2, "invalidFreq" : 0}, domain.counts_)
+		self.assertEqual({"minimum" : [1.0], "maximum" : [3.0], "mean" : [2.0], "standardDeviation" : [0.7071067811865476], "median" : [2.0], "interQuartileRange" : [0.5]}, _array_to_list(domain.numeric_info_))
+		self.assertEqual(numpy.array([1.0, 4.0, 3.0, 2.0, 4.0, 2.0]).tolist(), Xt[0].tolist())
+		X = numpy.array([-1.0, -1.0])
+		Xt = domain.transform(X)
+		self.assertEqual(numpy.array([4.0, 4.0]).tolist(), Xt.tolist())
 
 	def test_mapper(self):
 		domain = ContinuousDomain()
