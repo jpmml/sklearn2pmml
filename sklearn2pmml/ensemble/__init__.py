790225ea33a96f8f3992282e2ece3be41d11064d
Villu Ruusmann <villu.ruusmann@gmail.com>
2019-06-20 00:12:03 +0300

    Added 'GBDTLMRegressor' model type

--- sklearn2pmml/ensemble/__init__.py
+++ sklearn2pmml/ensemble/__init__.py
@@ -1,8 +1,25 @@
-from sklearn.base import clone, BaseEstimator, ClassifierMixin
-from sklearn.linear_model import LogisticRegression
+from sklearn.base import clone, BaseEstimator, ClassifierMixin, RegressorMixin
+from sklearn.linear_model import LinearRegression, LogisticRegression
 from sklearn.preprocessing import OneHotEncoder
 
-def _checkGBDT(gbdt):
+def _checkGBDTRegressor(gbdt):
+	if hasattr(gbdt, "apply"):
+		return gbdt
+	else:
+		try:
+			from lightgbm import LGBMRegressor
+			if isinstance(gbdt, LGBMRegressor):
+				return gbdt
+		except ImportError:
+			pass
+	raise ValueError("GBDT class " + gbdt.__class__ + " is not supported")
+
+def _checkLM(lm):
+	if isinstance(lm, LinearRegression):
+		return lm
+	raise ValueError("LM class " + lm.__class__ + " is not supported")
+
+def _checkGBDTClassifier(gbdt):
 	if hasattr(gbdt, "apply"):
 		return gbdt
 	else:
@@ -28,20 +45,19 @@ def _step_params(name, params):
 			del params[k]
 	return step_params
 
-class GBDTLRClassifier(BaseEstimator, ClassifierMixin):
+class GBDTEstimator(BaseEstimator):
 
-	def __init__(self, gbdt, lr):
-		self.gbdt = _checkGBDT(gbdt)
-		self.lr = _checkLR(lr)
+	def __init__(self, gbdt):
+		self.gbdt = gbdt
 
 	def _leaf_indices(self, X):
-		# Scikit-Learn classifiers
-		# XGBClassifier
+		# Scikit-Learn
+		# XGBoost
 		if hasattr(self.gbdt_, "apply"):
 			id = self.gbdt_.apply(X)
 			if id.ndim > 2:
 				id = id[:, :, 0]
-		# LGBMClassifier
+		# LightGBM
 		else:
 			id = self.gbdt_.predict(X, pred_leaf = True)
 		return id
@@ -50,6 +66,33 @@ class GBDTLRClassifier(BaseEstimator, ClassifierMixin):
 		id = self._leaf_indices(X)
 		idt = self.ohe_.transform(id)
 		return idt
+
+class GBDTLMRegressor(GBDTEstimator, RegressorMixin):
+
+	def __init__(self, gbdt, lm):
+		super(GBDTLMRegressor, self).__init__(_checkGBDTRegressor(gbdt))
+		self.lm = _checkLM(lm)
+
+	def fit(self, X, y, **fit_params):
+		self.gbdt_ = clone(self.gbdt)
+		self.gbdt_.fit(X, y, **_step_params("gbdt", fit_params))
+		id = self._leaf_indices(X)
+		self.ohe_ = OneHotEncoder(categories = "auto")
+		self.ohe_.fit(id)
+		idt = self.ohe_.transform(id)
+		self.lm_ = clone(self.lm)
+		self.lm_.fit(idt, y, **_step_params("lm", fit_params))
+		return self
+
+	def predict(self, X):
+		idt = self._encoded_leaf_indices(X)
+		return self.lm_.predict(idt)
+
+class GBDTLRClassifier(GBDTEstimator, ClassifierMixin):
+
+	def __init__(self, gbdt, lr):
+		super(GBDTLRClassifier, self).__init__(_checkGBDTClassifier(gbdt))
+		self.lr = _checkLR(lr)
 
 	def fit(self, X, y, **fit_params):
 		self.gbdt_ = clone(self.gbdt)
