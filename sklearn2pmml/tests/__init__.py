6d0811f98140da4457a5770919f9ee07d6ba54c3
Villu Ruusmann <villu.ruusmann@gmail.com>
2020-05-24 10:20:08 +0300

    Updated JPMML-SkLearn dependency

--- sklearn2pmml/tests/__init__.py
+++ sklearn2pmml/tests/__init__.py
@@ -88,9 +88,9 @@ class ClasspathTest(TestCase):
 
 	def test_classpath(self):
 		classpath = _classpath([])
-		self.assertEqual(22, len(classpath))
+		self.assertEqual(23, len(classpath))
 		classpath = _classpath(["A.jar", "B.jar"])
-		self.assertEqual(22 + 2, len(classpath))
+		self.assertEqual(23 + 2, len(classpath))
 
 	def test_supported_classes(self):
 		classes = _supported_classes([])
