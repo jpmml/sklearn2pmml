a01dec4919d83e1cf211d8119ffdaa573785dd52
Villu Ruusmann <villu.ruusmann@gmail.com>
2021-01-09 11:33:09 +0200

    Added 'Matcher' callable type
    See https://github.com/jpmml/jpmml-sklearn/issues/95#issuecomment-751786684

--- sklearn2pmml/feature_extraction/text/__init__.py
+++ sklearn2pmml/feature_extraction/text/__init__.py
@@ -13,6 +13,23 @@ for i in range(sys.maxunicode):
 	if (unicodedata.category(c)).startswith("P"):
 		punctuation += c
 
+class Matcher:
+
+	def __init__(self, word_re = "\w+"):
+		self.word_re = word_re
+
+	def __getstate__(self):
+		return self.word_re
+
+	def __setstate__(self, word_re):
+		self.word_re = word_re
+
+	def __call__(self, text):
+		tokens = re.findall(self.word_re, text)
+		# Remove empty tokens
+		tokens = [token for token in tokens if token]
+		return tuple(tokens)
+
 class Splitter:
 
 	def __init__(self, word_separator_re = "\s+"):
--- sklearn2pmml/feature_extraction/text/tests/__init__.py
+++ sklearn2pmml/feature_extraction/text/tests/__init__.py
@@ -1,8 +1,39 @@
 from io import BytesIO
-from sklearn2pmml.feature_extraction.text import Splitter
+from sklearn2pmml.feature_extraction.text import Matcher, Splitter
 from unittest import TestCase
 
 import joblib
+
+def _clone(x):
+	out_buf = BytesIO()
+	joblib.dump(x, out_buf)
+	out_buf.flush()
+	in_buf = BytesIO(out_buf.getvalue())
+	return joblib.load(in_buf)
+
+class MatcherTest(TestCase):
+
+	def test_call(self):
+		matcher = Matcher()
+		self.assertEqual((), matcher(""))
+		self.assertEqual((), matcher("."))
+		self.assertEqual(("one", ), matcher("one"))
+		self.assertEqual(("one", ), matcher("++one"))
+		self.assertEqual(("one", ), matcher("one++"))
+		self.assertEqual(("one", "two", "three"), matcher("one two three"))
+		self.assertEqual(("one", "_t", "w", "o_", "three"), matcher(",one _t,w.o_ three."))
+		matcher = Matcher("\w{2,}")
+		self.assertEqual(("one", "two", "three"), matcher("one two three"))
+		self.assertEqual(("one", "_t", "o_", "three"), matcher(",one _t,w.o_ three."))
+		matcher = Matcher("\w{4,}")
+		self.assertEqual(("three", ), matcher("one two three"))
+		self.assertEqual(("three", ), matcher(",one _t,w.o_ three."))
+
+	def test_pickle(self):
+		matcher = Matcher("\S+")
+		self.assertEqual("\S+", matcher.word_re)
+		matcher_clone = _clone(matcher)
+		self.assertEqual("\S+", matcher_clone.word_re)
 
 class SplitterTest(TestCase):
 
@@ -21,13 +52,5 @@ class SplitterTest(TestCase):
 	def test_pickle(self):
 		splitter = Splitter("\W")
 		self.assertEqual("\W", splitter.word_separator_re)
-		splitter_clone = SplitterTest._clone(splitter)
+		splitter_clone = _clone(splitter)
 		self.assertEqual("\W", splitter_clone.word_separator_re)
-
-	@staticmethod
-	def _clone(x):
-		out_buf = BytesIO()
-		joblib.dump(x, out_buf)
-		out_buf.flush()
-		in_buf = BytesIO(out_buf.getvalue())
-		return joblib.load(in_buf)
