d618fcb6b627d7044052a708af271eedf6d00f0b
Villu Ruusmann <villu.ruusmann@gmail.com>
2022-04-17 11:16:12 +0300

    Refactored 'BusinessDecisionTransformer' transformation type

--- sklearn2pmml/postprocessing/__init__.py
+++ sklearn2pmml/postprocessing/__init__.py
@@ -1,9 +1,10 @@
+from sklearn.base import clone, BaseEstimator, TransformerMixin
 from sklearn2pmml.preprocessing import ExpressionTransformer
 
-class BusinessDecisionTransformer(ExpressionTransformer):
+class BusinessDecisionTransformer(BaseEstimator, TransformerMixin):
 
-	def __init__(self, expr, business_problem, decisions, dtype = None):
-		super(BusinessDecisionTransformer, self).__init__(expr = expr, dtype = dtype)
+	def __init__(self, transformer, business_problem, decisions, prefit = False):
+		self.transformer = transformer
 		self.business_problem = business_problem
 		for decision in decisions:
 			if type(decision) is not tuple:
@@ -11,3 +12,17 @@ class BusinessDecisionTransformer(ExpressionTransformer):
 			if len(decision) != 2:
 				raise ValueError("Decision is not a two-element (value, description) tuple")
 		self.decisions = decisions
+		self.prefit = prefit
+		if prefit:
+			self.transformer_ = clone(transformer)
+
+	def fit(self, X, y = None):
+		self.transformer_ = clone(self.transformer)
+		if y is None:
+			self.transformer_.fit(X)
+		else:
+			self.transformer_.fit(X, y)
+		return self
+
+	def transform(self, X):
+		return self.transformer_.transform(X)
