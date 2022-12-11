14727d91c1e9ea1e596e001c184bdbc184124c76
Villu Ruusmann <villu.ruusmann@gmail.com>
2022-12-11 18:52:26 +0200

    Added 'OptimalBinningWrapper.metric' attribute

--- sklearn2pmml/preprocessing/optbinning.py
+++ sklearn2pmml/preprocessing/optbinning.py
@@ -4,10 +4,11 @@ from sklearn2pmml.util import ensure_1d
 
 class OptimalBinningWrapper(BaseEstimator, TransformerMixin):
 
-	def __init__(self, optimal_binning, prefit = False):
+	def __init__(self, optimal_binning, metric, prefit = False):
 		if not isinstance(optimal_binning, OptimalBinning):
 			raise TypeError("The optimal binning object is not an instance of {0}".format(OptimalBinning.__name__))
 		self.optimal_binning = optimal_binning
+		self.metric = metric
 		self.prefit = prefit
 		if self.prefit:
 			self.optimal_binning_ = clone(self.optimal_binning)
@@ -19,4 +20,4 @@ class OptimalBinningWrapper(BaseEstimator, TransformerMixin):
 
 	def transform(self, X):
 		X = ensure_1d(X)
-		return self.optimal_binning_.transform(X)
+		return self.optimal_binning_.transform(X, metric = self.metric)
