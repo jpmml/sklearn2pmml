c330bc0b78fef0523730cc39fa84263eb7ac2d23
Villu Ruusmann <villu.ruusmann@gmail.com>
2023-03-03 20:39:28 +0200

    Added 'ExpressionClassifier' model type

--- sklearn2pmml/expression/__init__.py
+++ sklearn2pmml/expression/__init__.py
@@ -1,5 +1,9 @@
-from sklearn.base import BaseEstimator, RegressorMixin
+from scipy.special import expit, softmax
+from sklearn.preprocessing import normalize
+from sklearn.base import BaseEstimator, ClassifierMixin, RegressorMixin
 from sklearn2pmml.util import eval_rows, to_expr_func, Expression
+
+import numpy
 
 class ExpressionRegressor(BaseEstimator, RegressorMixin):
 
@@ -14,3 +18,74 @@ class ExpressionRegressor(BaseEstimator, RegressorMixin):
 	def predict(self, X):
 		func = to_expr_func(self.expr)
 		return eval_rows(X, func, dtype = float)
+
+class ExpressionClassifier(BaseEstimator, ClassifierMixin):
+
+	def __init__(self, class_exprs, normalization_method):
+		if type(class_exprs) is not dict:
+			raise TypeError()
+		if not class_exprs:
+			raise ValueError()
+		for k, v in class_exprs.items():
+			if k is None:
+				raise ValueError()
+			if not isinstance(v, Expression):
+				raise TypeError()
+		self.class_exprs = class_exprs
+		normalization_methods = ["logit", "simplemax", "softmax"]
+		if normalization_method not in normalization_methods:
+			raise ValueError("Normalization method {0} not in {1}".format(normalization_method, normalization_methods))
+		self.normalization_method = normalization_method
+
+	def fit(self, X, y):
+		self.classes_ = numpy.unique(y)
+		n_classes = len(self.classes_)
+		classes = set(self.class_exprs.keys())
+		if n_classes == 2 and self.normalization_method == "logit":
+			if (len(classes) != 1) or (classes.intersection(self.classes_) != classes):
+				raise ValueError()
+		elif n_classes >= 2 and self.normalization_method in ["simplemax", "softmax"]:
+			if (len(classes) != n_classes) or (classes.intersection(self.classes_) != classes):
+				raise ValueError()
+		else:
+			raise ValueError()
+		return self
+
+	def decision_function(self, X):
+		y = None
+		for clazz in self.classes_:
+			try:
+				class_expr = self.class_exprs[clazz]				
+			except KeyError:
+				continue
+			class_func = to_expr_func(class_expr)
+			class_y = eval_rows(X, class_func, dtype = float)
+			if y is None:
+				y = class_y
+			else:
+				y = numpy.vstack((y, class_y))
+		if self.normalization_method in ["simplemax", "softmax"]:
+			y = y.T.reshape(X.shape[0], -1)
+		return y
+
+	def predict(self, X):
+		y_proba = self.predict_proba(X)
+		indices = numpy.argmax(y_proba, axis = 1)
+		return numpy.take(self.classes_, indices, axis = 0)
+
+	def predict_proba(self, X):
+		y = self.decision_function(X)
+		n_classes = len(self.classes_)
+		if n_classes == 2 and self.normalization_method == "logit":
+			proba = expit(y)
+			clazz = next(iter(self.class_exprs.keys()))
+			if (self.classes_).tolist().index(clazz) == 1:
+				return numpy.vstack((1 - proba, proba)).T
+			else:
+				return numpy.vstack((proba, 1 - proba)).T
+		elif n_classes >= 2 and self.normalization_method == "simplemax":
+			return normalize(y, norm = "l1", axis = 1)
+		elif n_classes >= 2 and self.normalization_method == "softmax":
+			return softmax(y, axis = 1)
+		else:
+			raise ValueError()
--- sklearn2pmml/expression/tests/__init__.py
+++ sklearn2pmml/expression/tests/__init__.py
@@ -1,5 +1,6 @@
 from pandas import DataFrame
-from sklearn2pmml.expression import ExpressionRegressor
+from scipy.special import expit
+from sklearn2pmml.expression import ExpressionClassifier, ExpressionRegressor
 from sklearn2pmml.util import Expression
 from unittest import TestCase
 
@@ -15,10 +16,94 @@ class ExpressionRegressorTest(TestCase):
 
 	def test_predict(self):
 		regressor = ExpressionRegressor(Expression("_relu(X[0])", function_defs = [_relu]))
-		X = numpy.array([[-2], [-1], [0], [1], [2]], dtype = int)
+		X = numpy.asarray([[-2], [-1], [0], [1], [2]], dtype = int)
 		y = regressor.predict(X)
 		self.assertEqual(float, y.dtype)
 		self.assertEqual([0, 0, 0, 1, 2], y.tolist())
 		X = DataFrame([-2.5, -1.5, -0.5, 0.5, 1.5, 2.5], columns = ["x"])
 		y = regressor.predict(X)
 		self.assertEqual([0.0, 0.0, 0.0, 0.5, 1.5, 2.5], y.tolist())
+
+def _is_negative(x):
+	if x < 0:
+		return 1
+	else:
+		return 0
+
+def _is_neutral(x):
+	if (x >= -0.5) and (x <= 0.5):
+		return 2
+	else:
+		return 0
+
+def _is_positive(x):
+	if x > 0:
+		return 1
+	else:
+		return 0
+
+class ExpressionClassifierTest(TestCase):
+
+	def test_binary_fit_predict(self):
+		class_exprs = {
+			"yes": Expression("X[0] ** X[1] + X[2]")
+		}
+		classifier = ExpressionClassifier(class_exprs, normalization_method = "logit")
+		X = numpy.asarray([[2, 1/2, 0], [2, 1, 0], [2, 2, -6]])
+		y = numpy.asarray(["yes", "yes", "no"])
+		classifier.fit(X, y)
+		self.assertEqual(["no", "yes"], classifier.classes_.tolist())
+		pred_dfs = classifier.decision_function(X)
+		self.assertEqual((3, ), pred_dfs.shape)
+		self.assertEqual([numpy.sqrt(2), 2, -2], pred_dfs.tolist())
+		preds = classifier.predict(X)
+		self.assertEqual(["yes", "yes", "no"], preds.tolist())
+		pred_probs = classifier.predict_proba(X)
+		self.assertEqual((3, 2), pred_probs.shape)
+		self.assertAlmostEqual(3, numpy.sum(pred_probs))
+		self.assertEqual([expit(numpy.sqrt(2)), expit(2), expit(-2)], pred_probs[:, 1].tolist())
+
+		class_exprs["no"] = Expression("1.0")
+		classifier = ExpressionClassifier(class_exprs, normalization_method = "softmax")
+		classifier.fit(X, y)
+		self.assertEqual(["no", "yes"], classifier.classes_.tolist())
+		pred_dfs = classifier.decision_function(X)
+		self.assertEqual((3, 2), pred_dfs.shape)
+		self.assertEqual([1, 1, 1], pred_dfs[:, 0].tolist())
+		self.assertEqual([numpy.sqrt(2), 2, -2], pred_dfs[:, 1].tolist())
+		preds = classifier.predict(X)
+		self.assertEqual(["yes", "yes", "no"], preds.tolist())
+		pred_probs = classifier.predict_proba(X)
+		self.assertEqual((3, 2), pred_probs.shape)
+		self.assertAlmostEqual(3, numpy.sum(pred_probs))
+
+	def test_multiclass_predict(self):
+		class_exprs = {
+			"negative" : Expression("_is_negative(X[0])", function_defs = [_is_negative]),
+			"neutral" : Expression("_is_neutral(X[0])", function_defs = [_is_neutral]),
+			"positive" : Expression("_is_positive(X[0])", function_defs = [_is_positive])
+		}
+		classifier = ExpressionClassifier(class_exprs, normalization_method = "simplemax")
+		X = numpy.asarray([[-0.25], [-1], [1]])
+		y = numpy.asarray(["neutral", "negative", "positive"])
+		classifier.fit(X, y)
+		self.assertEqual(["negative", "neutral", "positive"], classifier.classes_.tolist())
+		pred_dfs = classifier.decision_function(X)
+		self.assertEqual((3, 3), pred_dfs.shape)
+		self.assertEqual([[1, 2, 0], [1, 0, 0], [0, 0, 1]], pred_dfs.tolist())
+		preds = classifier.predict(X)
+		self.assertEqual(["neutral", "negative", "positive"], preds.tolist())
+		pred_probs = classifier.predict_proba(X)
+		self.assertEqual((3, 3), pred_probs.shape)
+		self.assertAlmostEqual(3, numpy.sum(pred_probs))
+		self.assertEqual([[1.0 / 3.0, 2.0 / 3.0, 0], [1, 0, 0], [0, 0, 1]], pred_probs.tolist())
+
+		classifier.normalization_method = "softmax"
+		pred_probs = classifier.predict_proba(X)
+		self.assertAlmostEqual(3, numpy.sum(pred_probs))
+		exp_sum = (numpy.exp(0) + numpy.exp(1) + numpy.exp(2))
+		self.assertTrue(numpy.allclose([numpy.exp(1) / exp_sum, numpy.exp(2) / exp_sum, numpy.exp(0) / exp_sum], pred_probs[0, :].tolist()))
+		exp_off = (numpy.exp(0) / (2 * numpy.exp(0) + numpy.exp(1)))
+		exp_on = (numpy.exp(1) / (2 * numpy.exp(0) + numpy.exp(1)))
+		self.assertTrue(numpy.allclose([exp_on, exp_off, exp_off], pred_probs[1, :].tolist()))
+		self.assertTrue(numpy.allclose([exp_off, exp_off, exp_on], pred_probs[2, :].tolist()))
