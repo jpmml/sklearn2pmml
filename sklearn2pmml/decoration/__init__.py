1232f05c9fd42bf95a2ad37e55592417719d0aaa
Villu Ruusmann <villu.ruusmann@gmail.com>
2024-03-15 21:43:52 +0200

    Added 'Domain.feature_names_in_' attribute

--- sklearn2pmml/decoration/__init__.py
+++ sklearn2pmml/decoration/__init__.py
@@ -113,6 +113,11 @@ class Domain(BaseEstimator, TransformerMixin, OneToOneFeatureMixin):
 		self.dtype = dtype
 		self.display_name = display_name
 
+	def _check_input(self, X, reset):
+		self._check_n_features(X, reset = reset)
+		self._check_feature_names(X, reset = reset)
+		return X
+
 	def _empty_fit(self):
 		return not (self.with_data or self.with_statistics)
 
@@ -198,7 +203,7 @@ class Domain(BaseEstimator, TransformerMixin, OneToOneFeatureMixin):
 		return False
 
 	def transform(self, X):
-		self._check_n_features(X, reset = False)
+		self._check_input(X, reset = False)
 		if self.dtype is not None:
 			X = cast(X, self.dtype)
 		missing_mask, valid_mask, invalid_mask = self._compute_masks(X)
@@ -254,7 +259,7 @@ class DiscreteDomain(Domain):
 		return super(DiscreteDomain, self)._valid_value_mask(X, where)
 
 	def fit(self, X, y = None):
-		self._check_n_features(X, reset = True)
+		self._check_input(X, reset = True)
 		if self.dtype is not None:
 			if _is_proto_pandas_categorical(self.dtype):
 				if self.data_values is not None:
@@ -367,7 +372,7 @@ class ContinuousDomain(Domain):
 		return super(ContinuousDomain, self)._valid_value_mask(X, where)
 
 	def fit(self, X, y = None):
-		self._check_n_features(X, reset = True)
+		self._check_input(X, reset = True)
 		if self.dtype is not None:
 			X = cast(X, self.dtype)
 		self.dtype_ = common_dtype(X)
@@ -455,7 +460,7 @@ class TemporalDomain(Domain):
 			raise ValueError("Temporal data type {0} not in {1}".format(dtype, dtypes))
 
 	def fit(self, X, y = None):
-		self._check_n_features(X, reset = True)
+		self._check_input(X, reset = True)
 		self.dtype_ = common_dtype(X)
 		return self
 
