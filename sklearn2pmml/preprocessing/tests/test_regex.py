694d829848c53a69f1dba60303d64c6dfeca7e22
Villu Ruusmann <villu.ruusmann@gmail.com>
2024-10-17 21:04:15 +0300

    Updated test resources

--- sklearn2pmml/preprocessing/tests/test_regex.py
+++ sklearn2pmml/preprocessing/tests/test_regex.py
@@ -4,15 +4,21 @@ from unittest import TestCase
 class PCRE2EngineTest(TestCase):
 
 	def test_matches(self):
-		engine = PCRE2Engine("ar?y")
+		engine = PCRE2Engine(r"ar?y")
 		self.assertTrue(engine.matches("January"))
 		self.assertFalse(engine.matches("March"))
 		self.assertTrue(engine.matches("May"))
+		engine = PCRE2Engine(r"r$")
+		self.assertFalse(engine.matches("March"))
+		self.assertTrue(engine.matches("October"))
 
 	def test_replace(self):
 		engine = PCRE2Engine(r"(\w)")
 		self.assertEqual("P u p p y", engine.replace(r"$1 ", "Puppy").rstrip())
 		self.assertEqual(r"\1 \1 \1 \1 \1", engine.replace(r"\1 ", "Puppy").rstrip())
+		with self.assertRaises(Exception):
+			engine.replace(r"$", "Puppy")
+		self.assertEqual("$$$$$", engine.replace(r"$$", "Puppy"))
 
 class REEngineTest(TestCase):
 
@@ -24,8 +30,12 @@ class REEngineTest(TestCase):
 		self.assertFalse(engine.matches("April"))
 		self.assertTrue(engine.matches("May"))
 		self.assertFalse(engine.matches("June"))
+		engine = REEngine(r"r$")
+		self.assertFalse(engine.matches("March"))
+		self.assertTrue(engine.matches("October"))
 
 	def test_replace(self):
 		engine = REEngine(r"(\w)")
 		self.assertEqual("$1 $1 $1 $1 $1", engine.replace(r"$1 ", "Puppy").rstrip())
 		self.assertEqual("P u p p y", engine.replace(r"\1 ", "Puppy").rstrip())
+		self.assertEqual("$$$$$", engine.replace(r"$", "Puppy"))
