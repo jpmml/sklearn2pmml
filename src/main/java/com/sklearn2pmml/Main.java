4ca21ff067ac720cd156203d20591b3547f567c5
Villu Ruusmann <villu.ruusmann@gmail.com>
2024-10-28 22:56:56 +0200

    Added logging configuration

--- src/main/java/com/sklearn2pmml/Main.java
+++ src/main/java/com/sklearn2pmml/Main.java
@@ -20,9 +20,11 @@ package com.sklearn2pmml;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.OutputStream;
 import java.util.Collection;
 import java.util.List;
+import java.util.logging.LogManager;
 
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
@@ -144,6 +146,16 @@ public class Main extends Application {
 			try(OutputStream os = new FileOutputStream(this.outputFile)){
 				MetroJAXBUtil.marshalPMML(pmml, os);
 			}
+		}
+	}
+
+	static {
+		LogManager logManager = LogManager.getLogManager();
+
+		try {
+			logManager.readConfiguration(Main.class.getResourceAsStream("/logging.properties"));
+		} catch(IOException ioe){
+			ioe.printStackTrace(System.err);
 		}
 	}
 
