a3db51c5b5a73a335d77c65a4fc7849f6f2b2897
Villu Ruusmann <villu.ruusmann@gmail.com>
2024-12-07 09:21:35 +0200

    Refactored package classpath

--- sklearn2pmml/resources/__init__.py
+++ sklearn2pmml/resources/__init__.py
@@ -1,12 +1,14 @@
-from importlib.resources import path
+from pathlib import Path
 
 def _package_classpath():
+	sklearn2pmml_dir = Path(__import__("sklearn2pmml").__file__).parent
+	resources_dir = sklearn2pmml_dir / "resources"
+
 	jars = []
-	with path("sklearn2pmml.resources", "classpath.txt") as classpath_file:
-		resources_dir = classpath_file.parent
-		with open(str(classpath_file), "r") as classpath:
-			jar_names = [line.strip("\n") for line in classpath.readlines()]
-			for jar_name in jar_names:
-				jar_file = resources_dir / jar_name
-				jars.append(str(jar_file))
+	classpath_file = resources_dir / "classpath.txt"
+	with open(str(classpath_file), "r") as classpath:
+		jar_names = [line.rstrip("\n") for line in classpath.readlines()]
+		for jar_name in jar_names:
+			jar_file = resources_dir / jar_name
+			jars.append(str(jar_file))
 	return jars
