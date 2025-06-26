8840a76bf6225681b6b1ea3f3bd14b1308c0f8df
Villu Ruusmann <villu.ruusmann@gmail.com>
2025-06-26 09:42:10 +0300

    Removed 'sklearn2pmml.pycaret.make_pmml_pipeline()' utility function
    See commit 35ba05b77f

--- sklearn2pmml/pycaret.py
+++ sklearn2pmml/pycaret.py
@@ -1,12 +1,8 @@
 from pycaret.internal.preprocess.transformers import TransformerWrapper
 from sklearn2pmml import _escape as _default_escape
-from sklearn2pmml import make_pmml_pipeline as default_make_pmml_pipeline
 
 def _escape(obj, escape_func):
 	obj = _default_escape(obj, escape_func = escape_func)
 	if isinstance(obj, TransformerWrapper):
 		obj.transformer = escape_func(obj.transformer, escape_func = escape_func)
 	return obj
-
-def make_pmml_pipeline(obj, active_fields = None, target_fields = None):
-	return default_make_pmml_pipeline(obj, active_fields = active_fields, target_fields = target_fields)
